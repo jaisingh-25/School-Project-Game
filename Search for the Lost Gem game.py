d1f9c670acbc0355682f2f25c5bf6cf5bf58e820
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2023-11-04 23:52:32 +0530

    Update Search for the Lost Gem game.py

--- Search for the Lost Gem game.py
+++ Search for the Lost Gem game.py
@@ -12,10 +12,8 @@ while b!="3" :
         print("Wrong answer, please try again")
 print("Yes, you are right. These are the possessions of a Wizard!")
 a=input("Please press the enter key to continue.\n")
-print("You search the house for further clues.\nUpon inspecting the fireplace, you find a secret note left by Pippin, which enlightens you about what happened the previous night.\nHe writes that at late night, an unknown entity broke into his home, whom you have successfully identified as the Wizard.")
-print("As Pippin tried to grab the intruder, he was incapacitated by the Wizard, who cast a spell of silence on him, so that Pippin won't be able to tell the Guards what happened.")
-a=input("Please press the enter key to continue.")
-print()
+print("You search the house for further clues.\nUpon inspecting the fireplace, you find a secret note left by Pippin, which enlightens you about what happened the previous night.\nHe writes that at late night, an unknown entity broke into his home, whom you have successfully identified as the Wizard.\nAs Pippin tried to grab the intruder, he was incapacitated by the Wizard, who cast a spell of silence on him, so that Pippin won't be able to tell the Guards what happened.")
+a=input("Please press the enter key to continue.\n")
 print("In his unexpected confrontation with the Wizard, Pippin had managed to snatch \
 the hat, pipe and some hair off before the Wizard turned invisible and escaped.")
 print("You understand that the Wizard's plan was to frame Pippin in order to get \
