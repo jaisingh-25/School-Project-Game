5c4233ab10def6a93a4bcca038bea8d87fd6a61b
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-02-12 00:00:47 +0530

    Update Search for the Lost Gem game.py

--- Search for the Lost Gem game.py
+++ Search for the Lost Gem game.py
@@ -61,8 +61,7 @@ while j!="Continue" :
         j="Continue"
     else :
         j="Try Again"
-print("Before leaving the lair, the Centaur also hands you a special anti-magic potion which can be used to remove any magical properties or spells on an object.")
-print("You may use that in case you ever have to defend yourself or solve a puzzle.\nYou thank the Centaur and leave.")
+print("Before leaving the lair, the Centaur also hands you a special anti-magic potion which can be used to remove any magical properties or spells on an object.\nYou may use that in case you ever have to defend yourself or solve a puzzle.\nYou thank the Centaur and leave.")
 a=input("Please press the enter key to continue.")
 k="No"
 while k=="No" :
