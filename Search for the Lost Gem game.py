485059d9ac69f6d341c6cfa7c5c274ee8c9abb9a
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-02-13 23:58:47 +0530

    Update Search for the Lost Gem game.py

--- Search for the Lost Gem game.py
+++ Search for the Lost Gem game.py
@@ -68,8 +68,7 @@ while k=="No" :
     print("\nCHAPTER V - THE SIDE MISSION\nAs you are on your way to the Inn, you spot a small kid crying.\nYou approach her and come to know that she has lost her way home while she was chasing a deer.\nAs there is no one around, she asks for your help.\nIt might be a trap, might not be a trap...")
     k=input("How do you respond, Yes (to help) or No (to not help) ? - ")
     if k=="Yes" :
-        print("You ask her which turns she took and after how much distance.")
-        print("She replies - ")
+        print("You ask her which turns she took and after how much distance.\nShe replies - ")
         print()
         print("First she went straight for 100 m, after which she took three clockwise \
 90⁰ turns and went straight for another 100 m.")
