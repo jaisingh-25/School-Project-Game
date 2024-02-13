8bc96742bd309c7f5cb880ce6209a0cb2383046d
jaisingh-25 <113584679+jaisingh-25@users.noreply.github.com>
2024-02-13 23:58:27 +0530

    Update Search for the Lost Gem game.py

--- Search for the Lost Gem game.py
+++ Search for the Lost Gem game.py
@@ -65,8 +65,7 @@ print("Before leaving the lair, the Centaur also hands you a special anti-magic 
 a=input("Please press the enter key to continue.")
 k="No"
 while k=="No" :
-    print("\nCHAPTER V - THE SIDE MISSION\nAs you are on your way to the Inn, you spot a small kid crying.\nYou approach her and come to know that she has lost her way home while she was chasing a deer.")
-    print("As there is no one around, she asks for your help.\nIt might be a trap, might not be a trap...")
+    print("\nCHAPTER V - THE SIDE MISSION\nAs you are on your way to the Inn, you spot a small kid crying.\nYou approach her and come to know that she has lost her way home while she was chasing a deer.\nAs there is no one around, she asks for your help.\nIt might be a trap, might not be a trap...")
     k=input("How do you respond, Yes (to help) or No (to not help) ? - ")
     if k=="Yes" :
         print("You ask her which turns she took and after how much distance.")
